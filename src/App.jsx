d7d30fb4f502cc693df5b5cfdc35d68dd9e3e09c
jose12010 <josedavid12010123@gmail.com>
2022-09-26 16:34:19 -0500

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useLayoutEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+import WeatherData from './components/WeatherData'
+
+function App() {
+  const [position, setPosition] = useState()
+  let [weather, setWeather] = useState()
+  
+  
+    useEffect(() => {
+      
+      const coordinates = (pos) => {
+        
+        setPosition( 
+        
+          {"lat": pos.coords.latitude,
+           "lon": pos.coords.longitude
+          } 
+          
+        )
+      }
+
+      navigator.geolocation.getCurrentPosition(coordinates)
+    },[])
+
+ 
+    useEffect(() => {
+      const API = "ee4c58dc44cc8898d3029a2045e2615f"
+      if(position){
+        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.lat}&lon=${position.lon}&appid=${API}`
+        // const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${52.021111}&lon=${8.534722}&appid=${API}`
+        axios.get(URL)
+        .then( res => setWeather(res.data)) 
+        .catch( error => console.log(error))
+      }
+    },[position])
+
+    
+
+  return (
+    <div className="App">
+      <WeatherData
+        weather = {weather}
+      />  
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherData.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react"
+import imageCloud from "/Images/backgrounds/hand-painted-watercolor-pastel-sky-cloud-background_41066-1919.jpg"
+import imageRain from "/Images/backgrounds/rainy.jpg"
+import imageSun from "/Images/backgrounds/screen-6.webp"
+const WeatherData = ({weather}) => {
+    console.log(weather)
+    const [changetemp, setChangetemp] = useState(true)
+    const [changeback, setChangeback] = useState()
+    
+    function changeTempMeasure() {
+        if(changetemp) {
+            setChangetemp(false)
+            
+        }
+        if(!changetemp) {
+            setChangetemp(true)
+            
+        }
+        
+    }
+
+    useEffect(()=>{
+        setBackground();
+        function setBackground(){
+            if(weather?.weather[0].main == "Clouds"){setChangeback(imageCloud)}
+            if(weather?.weather[0].main == "Rain"){setChangeback(imageRain)}
+            if(weather?.weather[0].main == "Clear"){setChangeback(imageSun)}
+            
+        }
+    },[weather])
+    
+    return(
+        
+        <div className="main" style={{ backgroundImage: `url(${changeback})` }} >
+            <div className="display" >
+                
+            <div className="title">
+                <h1>{weather?.name}, {weather?.sys.country}</h1>
+            </div>
+            
+            <div className="weather_conditions">
+                <div className="img">
+                    <img src={ weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}alt="" />
+                </div>
+                <div className="info">
+                    <p>Temperature: {changetemp == true ? `${Math.round((weather?.main.temp-273.15)*(9/5)+32)} F°` : `${Math.round(weather?.main.temp - 273.15)} C°`}</p>
+                    <p>Humidity: {`${weather?.main.humidity} %`}</p>
+                    <p>Thermal sensation  : {changetemp == true ? `${Math.round((weather?.main.feels_like-273.15)*(9/5)+32)} F°` : `${Math.round(weather?.main.feels_like - 273.15)} C°`}</p>
+                    <p>Wind: {weather?.wind.speed} m/s at {weather?.wind.deg}° </p>
+                </div>
+                <button className="button" onClick={changeTempMeasure}>
+                    Change
+                </button>
+            </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default WeatherData 
